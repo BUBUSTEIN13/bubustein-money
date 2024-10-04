1ed752011c4e56ecba6c7f5ef4de8a2ccb0060a5
BUBUSTEIN <bubustein.official@gmail.com>
2024-10-04 20:03:40 +0300

    Update BankMachineCategory.java

--- common/src/main/java/tk/bubustein/money/compat/jei/BankMachineCategory.java
+++ common/src/main/java/tk/bubustein/money/compat/jei/BankMachineCategory.java
@@ -3,11 +3,13 @@ package tk.bubustein.money.compat.jei;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -21,10 +23,8 @@ import java.util.List;
 
 public class BankMachineCategory implements IRecipeCategory<BankMachineRecipe> {
     public static final RecipeType<BankMachineRecipe> RECIPE_TYPE = RecipeType.create(MoneyMod.MOD_ID, "bank_machine", BankMachineRecipe.class);
-    private final IDrawable background;
     private final IDrawable icon;
     public BankMachineCategory(IGuiHelper guiHelper) {
-        this.background = guiHelper.createDrawable(ResourceLocation.fromNamespaceAndPath(MoneyMod.MOD_ID, "textures/container/bank_machine.png"), 29, 16, 116, 54);
         this.icon = guiHelper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.BANK_MACHINE.get()));
     }
     @Override
@@ -36,8 +36,16 @@ public class BankMachineCategory implements IRecipeCategory<BankMachineRecipe> {
         return BankMachine.TITLE;
     }
     @Override
-    public @NotNull IDrawable getBackground() {
-        return background;
+    public void draw(BankMachineRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
+        guiGraphics.blit(ResourceLocation.fromNamespaceAndPath(MoneyMod.MOD_ID, "textures/container/bank_machine.png"), 0, 0, 29, 16, getWidth(), getHeight(), 256, 256);
+    }
+    @Override
+    public int getWidth() {
+        return 116;
+    }
+    @Override
+    public int getHeight() {
+        return 54;
     }
     @Override
     public @NotNull IDrawable getIcon() {
